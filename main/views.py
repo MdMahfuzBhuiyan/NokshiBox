a1ced263f068e01cfb495e607523d57a35a2d9e3
Md. Mahfuz Bhuiyan <codomahfuz@gmail.com>
2025-09-14 13:34:56 +0600

    1st

---
+++ main/views.py
@@ -0,0 +1,6 @@
+#This is a module & we are having a lot of functions
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home (request):
+    return HttpResponse("Bismillah")
